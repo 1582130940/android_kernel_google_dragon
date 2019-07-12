b16afc5cc97f8c90d9a48656afaccc7408158330
1582130940 <1582130940@qq.com>
2019-07-12 14:55:48 +0800

    uapi: msg: Increase the value of the default message queue

--- include/uapi/linux/msg.h
+++ include/uapi/linux/msg.h
@@ -60,9 +60,9 @@ struct msginfo {
  */
 #define MSG_MEM_SCALE 32
 
-#define MSGMNI    16   /* <= IPCMNI */     /* max # of msg queue identifiers */
-#define MSGMAX  8192   /* <= INT_MAX */   /* max size of message (bytes) */
-#define MSGMNB 16384   /* <= INT_MAX */   /* default max size of a message queue */
+#define MSGMNI    32   /* <= IPCMNI */     /* max # of msg queue identifiers */
+#define MSGMAX 16384   /* <= INT_MAX */   /* max size of message (bytes) */
+#define MSGMNB 65536   /* <= INT_MAX */   /* default max size of a message queue */
 
 /* unused */
 #define MSGPOOL (MSGMNI * MSGMNB / 1024) /* size in kbytes of message pool */
